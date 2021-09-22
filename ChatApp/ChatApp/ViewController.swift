82467b95dc307eb9f2d3973c5ae3da9f52618cc4
taymasoff <taymas97@gmail.com>
2021-09-22 05:21:12 +0300

    init + gitignore
    Первоначальный сетап проекта, с добавлением файлов Readme и gitignore

---
+++ ChatApp/ChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Тимур Таймасов on 22.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
