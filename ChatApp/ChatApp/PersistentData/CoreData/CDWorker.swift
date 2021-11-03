1d6292ebf82d0aa796e1763b87e26ce63d4b3b7d
taymasoff <taymas97@gmail.com>
2021-11-03 05:48:37 +0300

    Создал CoreDataWorker (CDWorker)
    CDWorker юнит стака операций. На инициалиации принимает контекст, в котором проводить операции и mergePolicy. Передает контекст в coreDataManager, в котором будут проводиться все операции, затем по вызову метода saveIfNeeded контекст сохраняется, если в этом есть необходимость

---
+++ ChatApp/ChatApp/PersistentData/CoreData/CDWorker.swift
@@ -0,0 +1,45 @@
+//
+//  CDWorker.swift
+//  ChatApp
+//
+//  Created by Тимур Таймасов on 02.11.2021.
+//
+
+import Foundation
+import CoreData
+
+final class CDWorker<
+    Model: DomainModel,
+    Entity: NSManagedObject & ToDomainModelConvertable
+> where Model.Entity == Entity, Entity.DomainModel == Model {
+    
+    private let context: NSManagedObjectContext
+    
+    private var hasPersistentChanges: Bool {
+        return !context.insertedObjects.isEmpty || !context.deletedObjects.isEmpty || context.updatedObjects.contains(where: { $0.hasPersistentChangedValues })
+    }
+    
+    let coreDataManager: CoreDataManager<Model, Entity>
+    
+    init(context: NSManagedObjectContext,
+         mergePolicy: NSMergePolicyType = .mergeByPropertyObjectTrumpMergePolicyType) {
+        self.context = context
+        self.context.mergePolicy = NSMergePolicy(merge: mergePolicy)
+        self.coreDataManager = CoreDataManager<Model, Entity>(context: context)
+    }
+    
+    @discardableResult public func saveIfNeeded() -> Bool {
+        let hasPurpose = context.parent != nil || context.persistentStoreCoordinator?.persistentStores.isEmpty == false
+        guard hasPersistentChanges && hasPurpose else {
+            return false
+        }
+        do {
+            try context.save()
+            return true
+        } catch {
+            context.rollback()
+            Log.error("🗄 [CoreData]: Failed to save context: \(error)")
+            return false
+        }
+    }
+}
