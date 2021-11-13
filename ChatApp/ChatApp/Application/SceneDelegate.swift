833fadbf9db2585854fead910c9bb00d6ab26b4d
taymasoff <taymas97@gmail.com>
2021-11-13 03:55:06 +0300

    Интегрировал AppAssembler в приложение, почистил роутер

--- ChatApp/ChatApp/Application/SceneDelegate.swift
+++ ChatApp/ChatApp/Application/SceneDelegate.swift
@@ -12,17 +12,25 @@ import Rswift
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private lazy var diContainer = DIContainer()
 
     // MARK: - UIWindowSceneDelegate Lifecycle Methods
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
+        let appAssembler = AppAssembler(contaier: diContainer)
+        appAssembler.assembleMainRouter()
+        createAndShowFirstScene(scene: scene,
+                                router: diContainer
+                                    .resolve(type: MainRouter.self,
+                                             asSingleton: true))
+    }
+    
+    private func createAndShowFirstScene(scene: UIScene, router: MainRouterProtocol) {
         guard let windowScene = scene as? UIWindowScene else { return }
         window = UIWindow(windowScene: windowScene)
         window?.backgroundColor = ThemeManager.currentTheme.settings.mainColor
-        let navController = UINavigationController()
-        let router = MainRouter(navigationController: navController)
         router.initiateFirstViewController()
-        window?.rootViewController = navController
+        window?.rootViewController = router.navigationController
         window?.makeKeyAndVisible()
     }
 }
