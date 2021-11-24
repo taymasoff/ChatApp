8f47668e64ec3779af16e1a1b10eb3719ac39bd1
taymasoff <taymas97@gmail.com>
2021-11-24 07:07:37 +0300

    Создал NetworkError
    прописал ему все localizedDescriptions

---
+++ ChatApp/ChatApp/Core/Common/ErrorTypes/NetworkError.swift
@@ -0,0 +1,65 @@
+//
+//  NetworkError.swift
+//  ChatApp
+//
+//  Created by Тимур Таймасов on 23.11.2021.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    /// Не получилось создать Request или он не валиден
+    case badInput(String?)
+    /// Сервер не вернул никаких данных
+    case noData
+    /// Request отвергнут сервером (400-499)
+    case requestFailed(String?)
+    /// Ошибка на стороне сервера (500-599)
+    case serverError(String?)
+    /// Ошибка при парсинге данных
+    case parsingError(String?)
+    /// Неизвестная ошибка
+    case unknown
+}
+
+// MARK: - Localized Descriptions
+extension NetworkError: LocalizedError {
+    var localizedDescription: String? {
+        switch self {
+        case .badInput(let message):
+            return NSLocalizedString(
+                "📲 Не удалось собрать валидный запрос. "
+                    .appending(message ?? ""),
+                comment: "Bad Input"
+            )
+        case .noData:
+            return NSLocalizedString(
+                "📭 Сервер не вернул никаких данных.",
+                comment: "No Data"
+            )
+        case .requestFailed(let message):
+            return NSLocalizedString(
+                "❌ Запрос отвергнут сервером. "
+                    .appending(message ?? ""),
+                comment: "Request Failed"
+            )
+        case .serverError(let message):
+            return NSLocalizedString(
+                "♨️ Произошла ошибка на стороне сервера. "
+                    .appending(message ?? ""),
+                comment: "Server Error"
+            )
+        case .parsingError(let message):
+            return NSLocalizedString(
+                "🪄 Не удалось преобразовать данные. "
+                    .appending(message ?? ""),
+                comment: "Parsing Error"
+            )
+        case .unknown:
+            return NSLocalizedString(
+                "🤷‍♂️ Произошла неизвестная ошибка. ",
+                comment: "Unknown Error"
+            )
+        }
+    }
+}
