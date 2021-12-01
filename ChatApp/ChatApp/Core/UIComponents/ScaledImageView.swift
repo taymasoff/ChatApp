f38ae2e0735bb802c9330709f5ec5446c8c6ae2f
taymasoff <taymas97@gmail.com>
2021-12-01 08:00:00 +0300

    Создал ScaledImageView
    Решил вынести логику по подгонке правильной высоты ImageView с .aspectFit в кастомный ImageView, вместо обновления констрейнтов каждый раз

---
+++ ChatApp/ChatApp/Core/UIComponents/ScaledImageView.swift
@@ -0,0 +1,35 @@
+//
+//  ScaledImageView.swift
+//  ChatApp
+//
+//  Created by Тимур Таймасов on 01.12.2021.
+//
+
+import UIKit
+
+/// An image view that computes its intrinsic height from its width while preserving aspect ratio
+/// Source: https://stackoverflow.com/a/48476446
+class ScaledHeightImageView: UIImageView {
+    
+    // Track the width that the intrinsic size was computed for,
+    // to invalidate the intrinsic size when needed
+    private var layoutedWidth: CGFloat = 0
+
+    override var intrinsicContentSize: CGSize {
+        layoutedWidth = bounds.width
+        if let image = self.image {
+            let viewWidth = bounds.width
+            let ratio = viewWidth / image.size.width
+            return CGSize(width: viewWidth, height: image.size.height * ratio)
+        }
+        return super.intrinsicContentSize
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if layoutedWidth != bounds.width {
+            invalidateIntrinsicContentSize()
+        }
+    }
+}
